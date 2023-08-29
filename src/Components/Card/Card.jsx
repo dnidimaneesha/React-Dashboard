06e105b696968f017d4487bfa585b354bd03a8a5
dnidimaneesha <96563@fhss.sjp.ac.lk>
2023-08-29 10:28:23 +0530

    Create cards

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./Card.css";
+import { motion, AnimateSharedLayout } from "framer-motion";
+import { UilTimes } from "@iconscout/react-unicons";
+import Chart from "react-apexcharts";
 
-const Card = () => {
+// parent Card
+
+const Card = (props) => {
+  const [expanded, setExpanded] = useState(false);
   return (
-    <div>Card</div>
-  )
+    <AnimateSharedLayout>
+      {expanded ? (
+        "expended"
+      ) : (
+        <CompactCard param={props} setExpanded={() => setExpanded(true)} />
+      )}
+    </AnimateSharedLayout>
+  );
+};
+
+// Compact Card
+function CompactCard({ param, setExpanded }) {
+  const Png = param.png;
+  return (
+    <motion.div
+      className="CompactCard"
+      style={{
+        background: param.color.backGround,
+        boxShadow: param.color.boxShadow,
+      }}
+      layoutId="expandableCard"
+      onClick={setExpanded}
+    >
+      <div className="radialBar">
+        <span>{param.title}</span>
+      </div>
+      <div className="detail">
+        <Png />
+        <span>${param.value}</span>
+        <span>Last 24 hours</span>
+      </div>
+    </motion.div>
+  );
 }
 
-export default Card
+
+export default Card;
--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -1,27 +1,28 @@
-import React from 'react'
-import './Card.css'
-import { cardsData } from '../../Data/Data'
+import React from "react";
+import "./Cards.css";
+import { cardsData } from "../../Data/Data";
+
+import Card from "../Card/Card";
 
 const Cards = () => {
   return (
     <div className="Cards">
-    {cardsData.map((card, id) => {
-        return(
-            <div className="parentContainer">
-                <card
-                    title={card.title}
-                    color={card.color}
-                    barValue={card.barValue}
-                    value={card.value}
-                    png={card.png}
-                    series={card.series}
-                />
-            </div>
-        )
-    })}
-
+      {cardsData.map((card, id) => {
+        return (
+          <div className="parentContainer" key={id}>
+            <Card
+              title={card.title}
+              color={card.color}
+              barValue={card.barValue}
+              value={card.value}
+              png={card.png}
+              series={card.series}
+            />
+          </div>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default Cards
+export default Cards;
