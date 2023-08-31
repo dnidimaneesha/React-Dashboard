d4dc0145c4f3c9b294d3c7cc78626c9a72407a8a
dnidimaneesha <96563@fhss.sjp.ac.lk>
2023-08-31 11:08:13 +0530

    CreateUpdates and RightSide Component

---
+++ src/Components/RightSide/RightSide.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './RightSide.css'
+
+const RightSide = () => {
+  return (
+    <div className="RightSide">
+        <div>
+            <h3>Updates</h3>
+            <Updates/>
+            <div>Customer Review</div>
+        </div>
+    </div>
+  )
+}
+
+export default RightSide
