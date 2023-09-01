fe6762dec9b0bfbdb911ef0a2f76fa3a57a02051
dnidimaneesha <96563@fhss.sjp.ac.lk>
2023-09-01 10:32:53 +0530

    competed dashboard

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -2,15 +2,32 @@ import React, { useState } from 'react'
 import Logo from '../../imgs/logo.png';
 import "./Sidebar.css";
 import { SidebarData } from '../../Data/Data';
-import { UilSignOutAlt } from "@iconscout/react-unicons";
-
+import { UilSignOutAlt, UilBars } from "@iconscout/react-unicons";
+import { motion } from "framer-motion";
 
 const Sidebar = () => {
 
-  const [selected, setSelected] = useState(0)
+  const [selected, setSelected] = useState(0);
+  const [expanded, setExpaned] = useState(false);
+
+  const sidebarVariants = {
+    true:{
+      left: '0'
+    },
+    false:{
+      left:'-60%'
+    }
+  }
 
   return (
-    <div className="sidebar">
+    <>
+      <div className="bars" style={expanded?{left: '60%'}:{left: '5%'}} onClick={()=>setExpaned(!expanded)}>
+        <UilBars />
+      </div>
+      
+      <motion.div className='sidebar' variants={sidebarVariants}
+    animate={window.innerWidth<=768?`${expanded}`:''}
+    >
         {/* logo */}
          <div className="logo">
           <img src={Logo} alt=""/>
@@ -38,7 +55,8 @@ const Sidebar = () => {
 
           </div>
          </div>
-    </div>
+        </motion.div>
+    </>
   )
 }
 
